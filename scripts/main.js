4964366d15007804afaee2a8e2d459baa5530eb8
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2023-02-06 23:43:02 -0500

    updates to page

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+console.log("test!")
+
+// When the user clicks on <div>, open the popup
+function ethnographyPopUpA() {
+    var popup = document.getElementById("greenwood-ethnography-a");
+    popup.classList.toggle("show");
+}
+
+// When the user clicks on <div>, open the popup
+function ethnographyPopUpB() {
+    var popup = document.getElementById("greenwood-ethnography-b");
+    popup.classList.toggle("show");
+}
+
+// When the user clicks on <div>, open the popup
+function ethnographyPopUpC() {
+    var popup = document.getElementById("greenwood-ethnography-c");
+    popup.classList.toggle("show");
+}
+
+// When the user clicks on <div>, open the popup
+function ethnographyPopUpD() {
+    var popup = document.getElementById("greenwood-ethnography-d");
+    popup.classList.toggle("show");
+}
+
+// When the user clicks on <div>, open the popup
+function ethnographyPopUpE() {
+    var popup = document.getElementById("greenwood-ethnography-e");
+    popup.classList.toggle("show");
+}
